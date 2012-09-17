28331b8166f866e7d12020b8f7787bb02e21c36d
Kenta Kase <kesin1202000@gmail.com>
2012-09-17 11:58:29 +0900

    Add script that create mongodb database from csv
    and add found error article to error_list

---
+++ src/mongodb_insert.py
@@ -0,0 +1,29 @@
+#coding: utf-8
+'''
+wiki_placeからmongodbに追加する
+'''
+import csv
+from pymongo import Connection, GEO2D
+
+if __name__ == '__main__':
+    conn = Connection()
+    db = conn.test
+    fp = open('../wiki_place.csv', 'r')
+    places = csv.DictReader(fp, delimiter='|')
+    
+    for place in places:
+        lon = float(place['lng'])
+        lat = float(place['lat'])
+        if(lon < -180 or lon > 180):
+#            print place['title'], place
+            continue
+        if(lat < -180 or lat > 180):
+#            print place['title'], place
+            continue
+        db.wiki_place.save({'title':place['title'],
+                            'type':place['type'],
+                            'coord':[lon, lat]
+                            })
+    
+    db.wiki_place.ensure_index([('coord', GEO2D)])
+    fp.close()
