522657adaf5848e3ad6c629320597c632d32664c
Kenta Kase <kesin1202000@gmail.com>
2013-02-03 11:58:33 +0900

    Refactoring change some variable and method name

--- src/coordinate.py
+++ src/coordinate.py
@@ -14,13 +14,13 @@ import re
 import codecs
 import sys
 
-def get_place_info(title, lines):
+def get_places_info(title, lines):
     '''display=で記述されている座標情報から抽出'''
     places=[]
     for line in lines:
-        category_re = re.search('type:(.+?)[_|}|\(|\|]', line)
-        category = category_re.group(1) if category_re else ''
-        coord = get_coord(line)
+        CATEGORY_re = re.search('type:(.+?)[_|}|\(|\|]', line)
+        category = CATEGORY_re.group(1) if CATEGORY_re else ''
+        coord = str_to_coord(line)
         if coord:
             places.append({
                           'title': title,
@@ -29,11 +29,11 @@ def get_place_info(title, lines):
                           'lng': coord[1]})
     return places
         
-def get_place_info_jp(title, string):
+def get_places_info_jp(title, string):
     '''infobox内に| 緯度度 ... | 経度度 ...　
         で記述されている座標情報から抽出'''
     places=[]
-    coord = get_coord_jp(string)
+    coord = str_to_coord_jp(string)
     if coord:
         places.append({
                       'title': title,
@@ -42,7 +42,7 @@ def get_place_info_jp(title, string):
                       'lng': coord[1]})
     return places
             
-def get_coord(string):
+def str_to_coord(string):
     '''座標情報を取得する
        dms表記はdegree表記に変換する
        (南緯、西経)表記は(北緯、東経)表記に変換する
@@ -51,32 +51,34 @@ def get_coord(string):
     is_south = re.search('\|S', string)
     is_west = re.search('\|W', string)
     #dms表記
-    coord_re = re.search('\|(\d+\|\d+(\|?\d*?)?(\.\d+)?)\|[N,S]\|(\d+\|\d+(\|?\d*?)?(\.\d+)?)\|[E,W]', string)
-    if coord_re:
-        lat_dms = map((lambda x: float(x) if not x == '' else 0), coord_re.group(1).split('|'))
+    COORD_re = re.search('\|(\d+\|\d+(\|?\d*?)?(\.\d+)?)\|[N,S]\|(\d+\|\d+(\|?\d*?)?(\.\d+)?)\|[E,W]', string)
+    if COORD_re:
+        lat_dms = map((lambda x: float(x) if not x == '' else 0), COORD_re.group(1).split('|'))
         if len(lat_dms) < 3: lat_dms.append(0)
         lat_deg = lat_dms[0] + lat_dms[1]/60 + lat_dms[2]/3600
         
-        lng_dms = map((lambda x: float(x) if not x == '' else 0), coord_re.group(4).split('|'))
+        lng_dms = map((lambda x: float(x) if not x == '' else 0), COORD_re.group(4).split('|'))
         if len(lng_dms) < 3: lng_dms.append(0)
         lng_deg = lng_dms[0] + lng_dms[1]/60 + lng_dms[2]/3600
         
         if is_south: lat_deg *= -1
         if is_west: lng_deg *= -1
+
         return (lat_deg, lng_deg)
     
     #deg表記とdmsの度表記のみ
-    coord_re = re.search('\s?(-?\d+(\.\d+)?)\|([N,S]\|)?\s?(-?\d+(\.\d+)?)', string, re.UNICODE)
-    if coord_re:
-        lat_deg = float(coord_re.group(1))
-        lng_deg = float(coord_re.group(4))
+    COORD_re = re.search('\s?(-?\d+(\.\d+)?)\|([N,S]\|)?\s?(-?\d+(\.\d+)?)', string, re.UNICODE)
+    if COORD_re:
+        lat_deg = float(COORD_re.group(1))
+        lng_deg = float(COORD_re.group(4))
         if is_south: lat_deg *= -1
         if is_west: lng_deg *= -1
+
         return (lat_deg, lng_deg)
     
     return None
     
-def get_coord_jp(string):
+def str_to_coord_jp(string):
     """日本語のinfoboxでの少数派な記述方法
     | 緯度度 = 35 | 緯度分 = 38 | 緯度秒 = 3.8 | N(北緯)及びS(南緯) = N
     | 経度度 = 139 |経度分 = 47 | 経度秒 = 29.8 | E(東経)及びW(西経) = E
@@ -107,6 +109,7 @@ def get_coord_jp(string):
     
     if is_south: lat_deg *= -1
     if is_west: lng_deg *= -1
+
     return (lat_deg, lng_deg)
 
 if __name__ == '__main__':
@@ -123,22 +126,24 @@ if __name__ == '__main__':
     while line:
         #末尾の空白処理
         line = line.strip()
-        title_re = re.search('<title>(.+)</title>', line)
-        if title_re:
+        TITLE_re = re.search('<title>(.+)</title>', line)
+        if TITLE_re:
             #座標の抽出
             places=[]
-            places = get_place_info(title, page_lines)
+            places = get_places_info(title, page_lines)
             if page_lines_jp:
-                places += get_place_info_jp(title, '\n'.join(page_lines_jp))
+                places += get_places_info_jp(title, '\n'.join(page_lines_jp))
             
             for place in places:
                 string =  "%s|%s|%s|%s" % (place['title'],place['category'],place['lat'],place['lng'])
                 print string.encode('utf-8')
 
-            title = title_re.group(1)
+            title = TITLE_re.group(1)
             page_lines = []
             page_lines_jp = []
+
         #タイトル右上に表示されている座標
+        #この座標を最優先に使用する
         if re.search(u'display=.*title', line):
             page_lines.insert(0, line)
         #infobox内
@@ -146,6 +151,7 @@ if __name__ == '__main__':
             page_lines.append(line)
         elif re.search(u'^\|.+{{ウィキ座標.+}}$', line, re.UNICODE):
             page_lines.append(line)
+        #緯度度、経度度は2行に分かれているので一行進める
         elif re.search(u'緯度度\s*?=\s*?\d+', line, re.UNICODE):
             page_lines_jp.append(line)
             line = FILE.readline()
--- src/regex_test.py
+++ src/regex_test.py
@@ -16,28 +16,28 @@ class TestDecimalDegrees(unittest.TestCase):
         #総務省 landmark
         string = "{{Coord|35.675366|139.7511182|type:landmark_region:JP|display=inline,title|name=MIC}}"
         ans = (35.675366, 139.7511182)
-        self.assertEqual(ans, coordinate.get_coord(string))
+        self.assertEqual(ans, coordinate.str_to_coord(string))
 
     def test_dd_negative(self):
         #小数点マイナス（西系表示）
         #真珠湾
         string = "{{Coord|21.359255|-157.951813|format=dms|display=title}}"
         ans = (21.359255, -157.951813)
-        self.assertEqual(ans, coordinate.get_coord(string))
+        self.assertEqual(ans, coordinate.str_to_coord(string))
 
     def test_dd_space(self):
         #小数点 スペースを含む
         #築地
         string = "{{Coord|35.66819| 139.77390|format=dms|display=title}}"
         ans = (35.66819, 139.7739)
-        self.assertEqual(ans, coordinate.get_coord(string))
+        self.assertEqual(ans, coordinate.str_to_coord(string))
 
     def test_dd_NE(self):
         #小数点 NE区切り
         #ノートルダム大聖堂 (パリ) landmark
         string = "{{Coord|48.8530|N|2.3498|E|type:landmark_region:FR|display=title}}"
         ans = (48.8530, 2.3498)
-        self.assertEqual(ans, coordinate.get_coord(string))
+        self.assertEqual(ans, coordinate.str_to_coord(string))
 
 class TestDms(unittest.TestCase):
     '''度分秒(DMS)表記のテスト
@@ -54,20 +54,20 @@ class TestDms(unittest.TestCase):
         #黒海 waterbody
         string = "{{Coord|44|N|35|E|type:waterbody_scale:8000000 |display=title}}"
         ans = (44, 35)
-        self.assertEqual(ans, coordinate.get_coord(string))
+        self.assertEqual(ans, coordinate.str_to_coord(string))
 
     def test_dm_NE(self):
         #デルポイ landmark
         string = "{{Coord|38|29|N|22|30|E|type:landmark_region:GR|display=title}}"
         ans = (38.483333, 22.5)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_dms_NE(self):
         #欧州中央銀行
         string = "|coordinates = {{Coord|50|6|34|N|8|40|26|E|display=inline,title}}"
         ans = (50.109444, 8.673889)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_dms_float_NE(self):
@@ -75,7 +75,7 @@ class TestDms(unittest.TestCase):
         #203高地 
         string = "{{Coord|38|49|43.10|N|121|11|36.30|E|display=title}}"
         ans = (38.828639, 121.193417)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_dm_space_NE(self):
@@ -83,7 +83,7 @@ class TestDms(unittest.TestCase):
         #蒲郡市 city
         string = "{{Coord|34|50||N|137|13||E|region:JP_type:city|display=title}}"
         ans = (34.833333, 137.216667)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_dms_SE(self):
@@ -91,7 +91,7 @@ class TestDms(unittest.TestCase):
         #サボ島
         string = "{{Coord|09|08|00|S|159|49|00|E|display=title}}"
         ans = (-9.133333, 159.816667)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_dms_NW(self):
@@ -99,7 +99,7 @@ class TestDms(unittest.TestCase):
         #サンフランシスコ国際空港 airport 
         string = "| coordinates  = {{Coord|37|37|08|N|122|22|30|W|region:US-CA_type:airport|display=inline,title}}"
         ans = (37.618889, -122.375)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_d_SW(self):
@@ -107,7 +107,7 @@ class TestDms(unittest.TestCase):
         #南極点
         string = '<text xml:space="preserve">{{Coord|90|S|0|W|display=title}}'
         ans = (-90, 0)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
 class TestInfobox(unittest.TestCase):
@@ -119,7 +119,7 @@ class TestInfobox(unittest.TestCase):
     #内船駅
         string = u"""|座標 = {{coord|35|16|55.76|N|138|27|53.91|E|}}"""
         ans = (35.282156, 138.464975)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_wikicoord_dms(self):
@@ -127,7 +127,7 @@ class TestInfobox(unittest.TestCase):
         #ひょうたん島 (埼玉県)
         string = u"""|座標 = {{ウィキ座標度|36|7|52|N|139|0|28|E|}}"""
         ans = (36.131111, 139.007778)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_wikicoord_dm(self):
@@ -135,7 +135,7 @@ class TestInfobox(unittest.TestCase):
         #モンブラン
         string = u"""|座標={{ウィキ座標度分|45|50|00|N|6|55|00|E|}}"""
         ans = (45.833333, 6.916667)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_wikicoord_dms_float(self):
@@ -143,7 +143,7 @@ class TestInfobox(unittest.TestCase):
         #猪苗代駅
         string = u"""|座標 = {{ウィキ座標度分秒|37|32|46.73|N|140|6|11.08|E|}}"""
         ans = (37.546314, 140.103078)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
         
     def test_wikicoord2_dms_float(self):
@@ -151,7 +151,7 @@ class TestInfobox(unittest.TestCase):
         #新大阪駅
         string = u"""|座標      = {{ウィキ座標2段度分秒|34|44|0.54|N|135|30|0.41|E|type:railwaystation_region:JP}}"""
         ans = (34.733483, 135.500114)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord(string))
         self.assertEqual(ans, tuple(latlng))
 
 class TestInfoboxJP(unittest.TestCase):
@@ -164,7 +164,7 @@ class TestInfoboxJP(unittest.TestCase):
         string = u"""| 緯度度 = 35 | 緯度分 = 37 | 緯度秒 = 48.57 | N(北緯)及びS(南緯) = N 
 | 経度度 = 139 |経度分 = 47 | 経度秒 = 37.47 | E(東経)及びW(西経) = E"""
         ans = (35.630158, 139.793742)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord_jp(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord_jp(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_jp_nospace(self):
@@ -173,7 +173,7 @@ class TestInfoboxJP(unittest.TestCase):
         string = u"""|緯度度=34|緯度分=47|緯度秒=51.92
 |経度度=134|経度分=37|経度秒=41.31"""
         ans = (34.797756, 134.628142)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord_jp(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord_jp(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_jp_dm(self):
@@ -182,7 +182,7 @@ class TestInfoboxJP(unittest.TestCase):
         string = u"""|緯度度 = 41 |緯度分 = 54 |緯度秒 = |N(北緯)及びS(南緯) = N
 |経度度 = 87 |経度分 = 39 |経度秒 = |E(東経)及びW(西経) = W"""
         ans = (41.9, -87.65)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord_jp(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord_jp(string))
         self.assertEqual(ans, tuple(latlng))
 
     def test_jp_d(self):
@@ -191,7 +191,7 @@ class TestInfoboxJP(unittest.TestCase):
         string = u"""|緯度度 = 51.922832 |緯度分 = |緯度秒 = |N(北緯)及びS(南緯) = N
 |経度度 = 4.479606 |経度分 = |経度秒 = |E(東経)及びW(西経) = E"""
         ans = (51.922832, 4.479606)
-        latlng = map(lambda x: round(x, 6), coordinate.get_coord_jp(string))
+        latlng = map(lambda x: round(x, 6), coordinate.str_to_coord_jp(string))
         self.assertEqual(ans, tuple(latlng))
 
 if __name__ == '__main__':
